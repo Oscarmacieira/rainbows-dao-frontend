ecdc6ff0ae114e4f04d002b5c912ce8416a2028e
Oscar Macieira <macieiraoscar@icloud.com>
2022-08-13 11:13:09 +0200

    Fix type issues

--- components/branding/logo/icon/style.ts
+++ components/branding/logo/icon/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-export const LogoIconStyle = styled.img<{ small; medium }>`
+export const LogoIconStyle = styled.img<{ small: any; medium: any }>`
 	width: ${(props) =>
 		props?.small ? "30px" : props?.medium ? "60px" : "100px"};
 `;
